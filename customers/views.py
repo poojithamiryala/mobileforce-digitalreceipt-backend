f1269351b876029de8a0095996fa604542173276
fairtraxx <fairtraxx@protonmail.com>
2020-06-23 05:05:40 +0200

    updates/fixes/deploymentstuff/db.

--- customers/views.py
+++ customers/views.py
@@ -1,17 +1,25 @@
 import json
-from rest_framework import viewsets
+import datetime
+from rest_framework import viewsets, status
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import customers
 from .serializers import customersSerializer
-
+from rest_framework.decorators import api_view
 
 class indexViewSet(viewsets.ModelViewSet):
 	queryset = customers.objects.all().order_by('name')
 	serializer_class = customersSerializer
 
+@api_view(['GET'])
 def single(request, id):
-	customer = customers.objects.get(id = id)
-	return JsonResponse(customer, safe = False)
+	if request.method == 'GET':
+		try:
+			customer = customers.objects.get(id = id)
+			return JsonResponse(customer, safe = False)
+		except:
+			return JsonResponse({
+				'error': 'customer does not exist'
+				}, status = status.HTTP_400_BAD_REQUEST)
 		
 
