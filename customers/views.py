c9b7b8d037b666e60a146cf26fd5184b72aabecd
Bsaint <62014264+Bsaint95@users.noreply.github.com>
2020-06-23 09:15:11 +0100

    Update views.py

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+from rest_framework.decorators import api_view
 from rest_framework import viewsets
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -10,8 +12,24 @@ class indexViewSet(viewsets.ModelViewSet):
 	queryset = customers.objects.all().order_by('name')
 	serializer_class = customersSerializer
 
+@api_view(['GET'])	
 def single(request, id):
-	customer = customers.objects.get(id = id)
-	return JsonResponse(customer, safe = False)
+	if request.method == 'GET':
+		try:
+			customer = customers.objects.get(id = id)
+			return JsonResponse(customer, safe = False)
+		except:
+			return JsonResponse({
+				'error': 'customer does not exist'
+				}, status = status.HTTP_400_BAD_REQUEST)
+		
+@api_view(['POST'])
+def customer(request):
+    if request.method == 'POST':
+        issue_no = request.data['issue_no']
+        name = request.data['name']
+        email = request.data['email']
+        platform = request.data['platform']
+        return JsonResponse(issue_no, name, email, platform, safe = False)
 		
 
