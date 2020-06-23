be8dddec0d45ec6f4120a0f7163f27fb76437d19
Bsaint <62014264+Bsaint95@users.noreply.github.com>
2020-06-23 09:17:57 +0100

    Update urls.py

--- customers/urls.py
+++ customers/urls.py
@@ -8,6 +8,7 @@ router.register(r'customers', views.indexViewSet)
 urlpatterns = [
 	path('', include(router.urls)),
 	path('customers-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
+	path('new_customer/', customer),
 	path('customer/<int:id>/', views.single),
 
 ]
