48cc02faa6cefbf67041c0b0254aa1465adbc969
poojithamiryala <poojithamiryala@gmail.com>
2020-06-29 23:18:58 +0530

    user logout

--- userManagement/urls.py
+++ userManagement/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import user_registration_send_email, create_user, login, change_password,check_if_user_exists
+from .views import user_registration_send_email, create_user, login, change_password,check_if_user_exists,logout
 
 urlpatterns = [
     path('otp_register',user_registration_send_email),
     path('register', create_user),
     path('login', login),
     path('change_password',change_password),
+    path('logout', logout),
     path('email/exists',check_if_user_exists)
 ]
