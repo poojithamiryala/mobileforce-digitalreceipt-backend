bd80fe10de371007a7e228bd6d235543b7a2f33f
Adah Eni <eniwokepdm@gmail.com>
2020-06-29 12:50:16 +0100

    feat: add URL path for Draft receipts

--- businessManagement/urls.py
+++ businessManagement/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import create_receipt, add_product_info_to_receipt, get_all_receipt
+from .views import create_receipt, add_product_info_to_receipt, get_all_receipt, get_all_draft_receipt
 
 urlpatterns = [
     path('receipt/create',create_receipt),
     path('receipt/product',add_product_info_to_receipt),
-    path('receipt/history',get_all_receipt)
+    path('receipt/history',get_all_receipt),
+    path('receipt/draft',get_all_draft_receipt)
 ]
