e5595ef2d88ee5d1f70bfe3378e5a53c0a6056e9
Adah Eni <eniwokepdm@gmail.com>
2020-06-29 19:54:51 +0100

    feat: modify signature filed in Receipt

--- businessManagement/models.py
+++ businessManagement/models.py
@@ -13,7 +13,8 @@ class Receipts(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     receipt_number = models.CharField(
-        null=True, max_length=1000)  # need to customize this
+        null=True, max_length=1000
+    )  # need to customize this
     date = models.DateTimeField(null=True, default=datetime.now, blank=True)
     font = models.CharField(null=True, max_length=1000)
     color = models.CharField(null=True, max_length=1000)
@@ -23,18 +24,17 @@ class Receipts(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     issued = models.BooleanField(null=True, default=False)
     deleted = models.BooleanField(null=True, default=False)
-    signature = models.URLField(max_length=1000, null=True, default=False)
-    customer = models.ForeignKey(
-        CustomerDetails, on_delete=models.CASCADE, null=False)
+    signature = models.ImageField(upload_to="images/signatures", null=True)
+    customer = models.ForeignKey(CustomerDetails, on_delete=models.CASCADE, null=False)
 
 
 class Products(models.Model):
     receipt = models.ForeignKey(Receipts, on_delete=models.CASCADE, null=False)
     name = models.CharField(null=False, max_length=100)
     quantity = models.PositiveIntegerField(
-        null=False, validators=[MinValueValidator(1)])
-    unit_price = models.FloatField(
-        null=False, validators=[MinValueValidator(0)])
+        null=False, validators=[MinValueValidator(1)]
+    )
+    unit_price = models.FloatField(null=False, validators=[MinValueValidator(0)])
     part_payment = models.BooleanField(null=False, default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
